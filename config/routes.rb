e5da0b1b9d2a51a10bd6b674a9210b1d42ac5711
root <pixelmybit@gmail.com>
2019-06-07 10:56:32 +0200

    Added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, except: :index
   resources :cocktails, only: [:show, :create, :new] do
     resources :doses, only: [:new, :create]
   end
