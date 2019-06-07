20d1ff90aa874d24137bb466d54f03ace6b21539
root <pixelmybit@gmail.com>
2019-06-07 17:12:52 +0200

    all parts included

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -23,6 +23,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :dose_id)
+    params.require(:cocktail).permit(:name, :dose_id, :photo)
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -19,7 +19,7 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    # @dose.cocktail = Cocktail.find(params[:cocktail_id])
     @dose.destroy
 
     redirect_to cocktail_path(@dose.cocktail)
